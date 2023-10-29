0b895a6e07c0b31fff04e156c52046a800e48d3a
sebastianjb <bryant.sebastianj@gmail.com>
2023-10-29 14:10:33 -0500

    edited dashboard and details

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -5,6 +5,7 @@ import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
 import { useEffect } from 'react';
 import LoadingComponent from '../../../app/layout/LoadingComponents';
+import ActivityFilters from './ActivityFilters';
 
 
 
@@ -27,7 +28,7 @@ export default observer(function ActivityDashboard() {
             </Grid.Column>
 
             <Grid.Column width='6'>
-                <h2>Activity Filters</h2>
+                <ActivityFilters />
             </Grid.Column>
         </Grid>
     )
